57ac7b67dca7d6ee731bfd01c0088e893543b607
Alejandro Rosales <ayrosales09@gmail.com>
2026-06-01 19:25:29 -0600

    serializers.py: modified

--- CRM/apps/users/serializers.py
+++ CRM/apps/users/serializers.py
@@ -1,20 +1,13 @@
 from rest_framework import serializers
 
 from apps.organizations.models import Organization
+from apps.organizations.serializers import OrganizationSerializer
 
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True,
-        required=False,
-        style={'input_type': 'password'},
-    )
-    organization = serializers.PrimaryKeyRelatedField(
-        queryset=Organization.objects.all(),
-        required=False,
-    )
+class UserReadSerializer(serializers.ModelSerializer):
+    organization = OrganizationSerializer(read_only=True)
 
     class Meta:
         model = User
@@ -25,14 +18,33 @@ class UserSerializer(serializers.ModelSerializer):
             'email',
             'gender',
             'role',
-            'password',
         )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        request = self.context.get('request')
-        if request and not request.user.is_superuser:
-            self.fields['organization'].read_only = True
+
+class UserWriteSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=False,
+        style={'input_type': 'password'},
+    )
+    organization_id = serializers.PrimaryKeyRelatedField(
+        queryset=Organization.objects.all(),
+        source='organization',
+        required=False,
+        write_only=True,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'organization_id',
+            'full_name',
+            'email',
+            'gender',
+            'role',
+            'password',
+        )
 
     def validate(self, attrs):
         if self.instance is None and not attrs.get('password'):
@@ -42,16 +54,17 @@ class UserSerializer(serializers.ModelSerializer):
 
         request = self.context.get('request')
         organization = attrs.get('organization')
+
         if self.instance:
             organization = organization or self.instance.organization
         elif request and not request.user.is_superuser:
             attrs['organization'] = request.user.organization
         elif request and request.user.is_superuser and not organization:
             raise serializers.ValidationError(
-                {'organization': 'This field is required.'}
+                {'organization_id': 'This field is required.'}
             )
 
-        if self.instance and request and request.user.role != User.Role.ADMIN:
+        if self.instance and request and request.user.role != User.Role.ADMIN and not request.user.is_superuser:
             if 'role' in attrs and attrs['role'] != self.instance.role:
                 raise serializers.ValidationError(
                     {'role': 'Only admins can change roles.'}
@@ -62,7 +75,7 @@ class UserSerializer(serializers.ModelSerializer):
                 )
             if 'organization' in attrs and attrs['organization'] != self.instance.organization:
                 raise serializers.ValidationError(
-                    {'organization': 'Only admins can change organization.'}
+                    {'organization_id': 'Only admins can change organization.'}
                 )
 
         return attrs
